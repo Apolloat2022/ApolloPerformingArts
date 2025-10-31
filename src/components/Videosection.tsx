65033c58e3734f4f7a0564fd9a3eaa348866ee08
Apollo Technologies US <97480682+Apolloat2022@users.noreply.github.com>
2025-10-30 22:52:31 -0500

    Create Videosection.tsx

---
+++ src/components/Videosection.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Programs from './components/Programs';
+import VideoSection from './components/VideoSection'; // Add this back
+import Gallery from './components/Gallery';
+import Pricing from './components/Pricing';
+import TalentShow from './components/TalentShow';
+import Enrollment from './components/Enrollment';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Programs />
+        <VideoSection /> {/* Add this back */}
+        <Gallery />
+        <Pricing />
+        <TalentShow />
+        <Enrollment />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
