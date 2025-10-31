8729f7ddb76646d81a70991f7c6fde30ae8e775f
Apollo Technologies US <97480682+Apolloat2022@users.noreply.github.com>
2025-10-31 13:30:37 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,30 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Programs from './components/Programs';
-import VideoSection from './components/VideoSection'; // Add this import
+import Video from './components/Video';
 import Gallery from './components/Gallery';
 import Pricing from './components/Pricing';
 import TalentShow from './components/TalentShow';
 import Enrollment from './components/Enrollment';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './App.css';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <>
+    <div className="App">
       <Header />
-      <main>
-        <Hero />
-        <About />
-        <Programs />
-        <VideoSection /> {/* Add this line */}
-        <Gallery />
-        <Pricing />
-        <TalentShow />
-        <Enrollment />
-        <Contact />
-      </main>
+      <Hero />
+      <About />
+      <Programs />
+      <Video />
+      <Gallery />
+      <Pricing />
+      <TalentShow />
+      <Enrollment />
+      <Contact />
       <Footer />
-    </>
+    </div>
   );
-};
+}
 
 export default App;
