10b711fb736907ea27bff67ff6155f4a48486ec0
Apollo Technologies US <97480682+Apolloat2022@users.noreply.github.com>
2025-10-31 12:37:13 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,21 +20,16 @@ const Header: React.FC = () => {
       <div className="container">
         <nav className="navbar">
           <div className="logo">
-            {/* Logo Image - REMOVED FALLBACK */}
-            <div className="logo-img" style={{ border: '2px solid red' }}>
-              <img 
-                src="/Logo.png" 
-                alt="Apollo Performing Arts Logo" 
-                style={{ 
-                  width: '100%', 
-                  height: '100%', 
-                  objectFit: 'contain',
-                  borderRadius: '8px',
-                  border: '2px solid green'
-                }}
-              />
-              {/* REMOVED THE FALLBACK DIV */}
-            </div>
+            {/* Logo Image that fits the header */}
+            <img 
+              src="/Logo.png" 
+              alt="Apollo Performing Arts Logo" 
+              style={{ 
+                width: '50px',
+                height: '50px', 
+                objectFit: 'contain'
+              }}
+            />
             <span>Apollo Performing Arts</span>
           </div>
           <ul className={`nav-links ${isMenuOpen ? 'nav-active' : ''}`}>
